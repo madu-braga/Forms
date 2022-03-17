f6a368b08cd0164de135053784fdd1f52cf6fc75
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-03-17 20:22:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-hi
+const form = document.getElementsByID('form');
+const username = document.getElementsByID('username');
+const email = document.getElementsByID('email');
+const password = document.getElementsByID('password');
+const password2 = document.getElementsByID('password2');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    console.log(username);
+});
