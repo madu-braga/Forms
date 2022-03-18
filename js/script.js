4204cccfcdb30ab3d8e5169ae45c276f495f8af6
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-03-17 21:56:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,20 +4,49 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//show input error and sucess message
+//show input error message
 function showError(input, message){
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
+//show input sucess message
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check date is valid 
+function isValidEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 //Event Listener
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     if(username.value === ''){
         showError(username, 'Username is required');
-    }else{
+    } else if(!isValidEmail(email.value)){
+        showError(username, 'Username is not valid');
+    } else{ 
         showSuccess(username);
     }
-    console.log(username.value);
+    if(email.value === ''){
+        showError(email, 'email is required');
+    } else{
+        showSuccess(email);
+    }
+    if(password.value === ''){
+        showError(password, 'password is required');
+    } else{
+        showSuccess(password);
+    }
+    if(password2.value === ''){
+        showError(password2, 'password 2 is required');
+    } else{
+        showSuccess(password2);
+    }
 });
