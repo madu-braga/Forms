d44e7183f8f08c0d7695d2bc2aa41a94001d8ae9
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-08-01 15:27:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ function isValidEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
 //Event Listener
 form.addEventListener('submit', function(e){
     e.preventDefault();
